d6f21ddba9781cac6fe629752ebd5628ceabf0f7
fernandobituco <fernandoangelima@gmail.com>
2022-08-01 19:52:33 -0300

    search menu created

--- app/src/main/java/me/bituco/repositories/ui/MainActivity.kt
+++ app/src/main/java/me/bituco/repositories/ui/MainActivity.kt
@@ -2,11 +2,35 @@ package me.bituco.repositories.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import androidx.appcompat.widget.SearchView
 import me.bituco.repositories.R
+import me.bituco.repositories.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SearchView.OnQueryTextListener {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        val searchView = menu.findItem(R.id.action_search).actionView as SearchView
+        searchView.setOnQueryTextListener(this)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        Log.d("Search", "Text Submit $query")
+        return true
+    }
+
+    override fun onQueryTextChange(newText: String?): Boolean {
+        Log.d("Search", "Text Change $newText")
+        return false
     }
 }
